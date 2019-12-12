5ee4147dd4f144fdeb8488373c34d7b5ca843467
Nathalia Cristina <nathaliacristina20@hotmail.com>
2019-12-12 20:59:47 -0300

    Rota inicial

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { StatusBar } from 'react-native';
 import './config/ReactotronConfig';
 import { store, persistor } from './store';
-import Routes from './routes';
+import App from './App';
 
-export default function App() {
+export default function Index() {
     return (
         <Provider store={store}>
             <PersistGate persistor={persistor}>
                 <StatusBar batStyle="light-content" backgroundColor="#7159c1" />
-                <Routes />
+                <App />
             </PersistGate>
         </Provider>
     );
