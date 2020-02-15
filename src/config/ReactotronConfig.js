33ec6b3b70837ce945f0e9d53eac117601e6d88a
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-02-14 23:34:35 -0300

    Atualizacao de perfil

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -5,7 +5,7 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
     const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
-        .configure({ host: '192.168.0.12' })
+        .configure({ host: '192.168.56.1' })
         .useReactNative()
         .use(reactotronRedux())
         .use(reactotronSaga())
--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -5,11 +5,12 @@ import { updateProfileSuccess, updateProfileFailure } from './action';
 
 export function* updateProfile({ payload }) {
     try {
-        const { name, email, avatar_id, ...rest } = payload.data;
+        const { name, email, ...rest } = payload.data;
 
         const profile = {
-            name, email, avatar_id,
-            ...(rest.oldPassword ? rest : {})
+            name,
+            email,
+            ...(rest.oldPassword ? rest : {}),
         };
 
         const response = yield call(api.put, 'users', profile);
