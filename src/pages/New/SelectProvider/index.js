750dd5fa294d4359f751e657f67cf1b6a84fd47a
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-02-15 23:24:20 -0300

    Listagem de prestadores

--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
 
-export default function SelectProvider() {
-    return <Background />;
+import api from '~/services/api';
+
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
+
+export default function SelectProvider({ navigation }) {
+    const [providers, setProviders] = useState();
+
+    useEffect(() => {
+        async function loadProviders() {
+            const response = await api.get('providers');
+
+            setProviders(response.data);
+        }
+        loadProviders();
+    }, []);
+    return (
+        <Background>
+            <Container>
+                <ProvidersList
+                    data={providers}
+                    keyExtractor={provider => String(provider.id)}
+                    renderItem={({ item: provider }) => (
+                        <Provider
+                            onPress={() =>
+                                // eslint-disable-next-line react/prop-types
+                                navigation.navigate('SelectDateTime', {
+                                    provider,
+                                })
+                            }
+                        >
+                            <Avatar
+                                source={{
+                                    uri: provider.avatar
+                                        ? provider.avatar.url
+                                        : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+                                }}
+                            />
+                            <Name>{provider.name}</Name>
+                        </Provider>
+                    )}
+                />
+            </Container>
+        </Background>
+    );
 }
 
 SelectProvider.navigationOptions = ({ navigation }) => ({
