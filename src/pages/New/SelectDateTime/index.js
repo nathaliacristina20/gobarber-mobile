743bb71a8d6c6d63c249b276ac4720e6c14d83e5
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-02-16 12:45:06 -0300

    DatePicker por plataforma

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
-// import { Container } from './styles';
+import { Container } from './styles';
+
+import DateInput from '~/components/DateInput';
 
 export default function SelectDateTime() {
-    return <Background />;
+    const [date, setDate] = useState(new Date());
+    return (
+        <Container>
+            <Background>
+                <DateInput date={date} onChange={setDate} />
+            </Background>
+        </Container>
+    );
 }
 
-SelectDateTime.navigationOptions = {
+SelectDateTime.navigationOptions = ({ navigation }) => ({
     title: 'Selecione o horario',
-};
+    headerLeft: () => (
+        <TouchableOpacity
+            onPress={() => {
+                navigation.goBack();
+            }}
+        >
+            <Icon name="chevron-left" size={20} color="#FFF" />
+        </TouchableOpacity>
+    ),
+});
